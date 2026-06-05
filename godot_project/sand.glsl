a174459b44b557a95871eaba969975711539a2ec
Victor Martin <victor@MacBook-Air-de-Victor-2.local>
2026-06-05 17:46:15 +0200

    debug

--- godot_project/sand.glsl
+++ godot_project/sand.glsl
@@ -267,30 +267,24 @@ void four_cell(ivec2 cTL, ivec2 cTR, ivec2 cBL, ivec2 cBR) {
 		TR = 2;
 	}
 	bool coin = ((cTL.x * 3 + cTL.y * 7 + params.offset_x * 11) % 2) == 0;
-	if(BL == 2 && BR == 0){
-		if (coin){
-			BR = 2;
-			BL = 0;
-		}
-	}
-	else if(BL == 0 && BR == 2) {
-		if (coin){
-			BL = 2;
-			BR = 0;
-		}
-	}
-	if(TL == 2 && TR == 0) {
-		if (coin){
+	
+	// SUPPRIMÉ : Le mouvement horizontal pour BL et BR.
+	// On laisse la grille alternée (offset) s'en charger au tour suivant.
+
+	// AJOUTÉ : On s'assure que TL ou TR est soutenu (BL != 0 ou BR != 0) avant de s'étaler.
+	if (TL == 2 && TR == 0 && BL != 0) {
+		if (coin) {
 			TR = 2;
 			TL = 0;
 		}
 	}
-	else if(TL == 0 && TR == 2){
-		if (coin){
+	else if (TL == 0 && TR == 2 && BR != 0) {
+		if (coin) {
 			TL = 2;
 			TR = 0;
 		}
 	}
+
 	write_cell(cTL, float(TL));
 	write_cell(cTR, float(TR));
 	write_cell(cBL, float(BL));
